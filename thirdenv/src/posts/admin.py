30638ffff6268333016092140141eb16e43070f7
Adriana Bello <abellocabic@gmail.com>
2016-05-07 19:33:37 +0200

    absolute urls working

--- thirdenv/src/posts/admin.py
+++ thirdenv/src/posts/admin.py
@@ -3,7 +3,16 @@ from django.contrib import admin
 # import my Post class from models.py
 from .models import Post
 
+class PostModelAdmin(admin.ModelAdmin):
+	
+	list_display = ["__str__", "timestamp"]
+	list_filter = ["title", "timestamp"]
+	search_fields = ['title', 'content']
+	#list_editable = ['title']
+
+	class Meta:
+		model = Post
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostModelAdmin)
 
--- thirdenv/src/posts/views.py
+++ thirdenv/src/posts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from posts.models import Post
 
 # Create your views here.
+
+def post_create(request):
+	return HttpResponse("<h1>Create</h1>")
+
+def post_detail(request, id):
+	instance = get_object_or_404(Post, id=id)
+	context = {
+		'title' : instance.title,
+		'instance' : instance,
+	}
+	return render(request, "post_detail.html", context)
+
+def post_list(request):
+	queryset = Post.objects.all()
+	context = {
+		'title' : "List of Posts",
+		'object_list' : queryset,
+	}
+	return render(request, "index.html", context)
+
+def post_update(request):
+	return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request):
+	return HttpResponse("<h1>Delete</h1>")
